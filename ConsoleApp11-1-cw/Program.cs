edcd862c320fc0b5eb875f59fbfa4082fd525ab2
muxazzXJohny <maracoco334@gmail.com>
2024-12-09 22:55:43 +0200

    task-1-cw

---
+++ ConsoleApp11-1-cw/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11_1_cw
+{
+    /// <summary>
+    /// Поменять местами содержимое двух строковых переменных.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Value();
+        }
+        public static void Value()
+        {
+            string st1 = "Это первая строка";
+            string st2 = "Это вторая строка";
+            Console.WriteLine($"Вывод перед обменом:{st1}\t{st2}");
+            Swap(ref st1, ref st2);
+            Console.WriteLine($"Вывод после обмена:{st1}\t{st2}");
+            Console.ReadKey();
+        }
+
+        public static void Swap(ref string x, ref string y)
+        {
+            string p = x;
+            x = y;
+            y = p;
+        }
+    }
+}
---
+++ ConsoleApp11-1-cw/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp11-1-cw")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp11-1-cw")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4e9e6785-2a4f-420e-ab97-7c1f25e18a01")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
