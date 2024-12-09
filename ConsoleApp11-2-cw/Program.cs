21c00c8a61c2b1838035c5071f8beff73069cd59
muxazzXJohny <maracoco334@gmail.com>
2024-12-09 22:56:25 +0200

    task-2-cw

---
+++ ConsoleApp11-2-cw/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11_2_cw
+{
+    /// <summary>
+    /// Составить программу, в результате которой величина а меняется значением с величиной b, а величина c — с величиной d.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Value();
+        }
+        public static void Value()
+        {
+            int a = Input("Введите 1 число:");
+            int b = Input("Введите 2 число:");
+            int c = Input("Введите 3 число:");
+            int d = Input("Введите 4 число:");
+            Conclusion("До изменений", a, b, c, d);
+            Swap(ref a, ref b);
+            Swap(ref c, ref d);
+            Conclusion("После изменений", a, b, c, d);
+            Console.ReadKey();
+        }
+        public static void Swap(ref int a, ref int b)
+        {
+            int p = a;
+            a = b;
+            b = p;
+        }
+        public static int Input(string a)
+        {
+            Console.Write(a);
+            return int.Parse(Console.ReadLine());
+        }
+        public static void Conclusion(string message, int a, int b, int c, int d)
+        {
+            Console.WriteLine($"{message} {a} {b} {c} {d}");
+        }
+    }
+}
---
+++ ConsoleApp11-2-cw/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp11-2-cw")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp11-2-cw")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f28f8a65-fc0b-4a28-9146-ba18302c3450")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
